ae5f31fa99635cb296590250fb9e45b48d7b4172
cerlestes <mail@kevin-fischer.de>
2018-05-24 18:46:24 +0200

    improve configuration
    add keys 'align' and 'priority' to configuration, and switch from comma-separated strings to actual objects

--- src/shortcuts.ts
+++ src/shortcuts.ts
@@ -2,23 +2,33 @@ import {window, commands, workspace, StatusBarAlignment, StatusBarItem} from 'vs
 
 export class Shortcuts {
     private buttons: StatusBarItem[]
+    private align: 'left'|'right'
+    private priority: number
 
     constructor(config: any) {
+        const align = config.align;
+        const priority = config.priority;
         const btns = config.buttons;
+
+        if (align) {
+            this.align = align;
+        }
+        if (priority) {
+            this.priority = priority;
+        }
         if (btns) {
-            this.buttons = btns.map( (btnTx: string): StatusBarItem => {
-                const [icon,cmd,tip] = btnTx.split(',').map((tx: string): string => {return tx.trim()});
-                const btn = window.createStatusBarItem(StatusBarAlignment.Left);
-                btn.text=`$(${icon})`;
-                btn.command = cmd;
-                btn.tooltip = tip;
-                return btn;
-            })
+            this.buttons = btns.map( (btnTx: string|Object): StatusBarItem => {
+                if (typeof btnTx === 'object') {
+                    return this.fromObjectConfig(btnTx)
+                } else {
+                    return this.fromStringConfig(btnTx)
+                }
+            });
         }
     }
 
     show() {
-        this.buttons.forEach(btn=>{
+        this.buttons.forEach(btn => {
             btn.show();
         });
     }
@@ -27,5 +37,32 @@ export class Shortcuts {
         this.buttons.forEach(btn => {
             btn.dispose();
         });
+    }
+
+    private fromStringConfig(btnTx: string): StatusBarItem {
+        const [icon,cmd,tip] = btnTx.split(',').map((tx: string): string => {return tx.trim()});
+        const btn = window.createStatusBarItem(StatusBarAlignment.Left);
+        btn.text=`$(${icon})`;
+        btn.command = cmd;
+        btn.tooltip = tip;
+        return btn;
+    }
+
+    private fromObjectConfig(btnTx: any): StatusBarItem {
+        const { icon, command, tooltip, align, priority } = btnTx;
+        const btn = window.createStatusBarItem( this.alignFromString(align || this.align), (priority || this.priority) );
+        btn.text=`$(${icon})`;
+        btn.command = command;
+        btn.tooltip = tooltip;
+        return btn;
+    }
+
+    private alignFromString(align: string): StatusBarAlignment {
+        switch (align) {
+            case 'right':
+                return StatusBarAlignment.Right;
+            case 'left': default:
+                return StatusBarAlignment.Left;
+        }
     }
 }
